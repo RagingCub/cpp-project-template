38c91a784ad133160079d4200253e62d185227bd
RagingCub <RagingCub@users.noreply.github.com>
2021-06-14 17:25:22 -0400

    Add gettext and i18n

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include <libintl.h>
+#include "config.h"
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  setlocale(LC_ALL, "");
+  std::string binded = bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALEDIR);
+  std::string codeset = bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+  std::string text_domain = textdomain(GETTEXT_PACKAGE);
+
+  std::cout << gettext("Hello, World!") << std::endl;
 
   return 0;
 }
